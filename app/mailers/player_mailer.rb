3182ac38abf19844994b639dc982bf808934716f
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2018-02-24 16:38:57 -0500

    Adding cc

--- app/mailers/player_mailer.rb
+++ app/mailers/player_mailer.rb
@@ -7,6 +7,6 @@ class PlayerMailer < ApplicationMailer
     @from = pairing[:from]
     emails = (pairing[:to].map{|payment| payment[:player]} + pairing[:from].map{|payment| payment[:player]}).map{|player| player.email}
     # TODO fix this back up
-    mail(to: 'todo', subject: "Online Settlement #{Date.today.strftime("%B")} (Balance as of #{Date.today}) (Normally sent to #{emails.join(',')}")
+    mail(to: 'todo', cc: secrets["ccEmail"], subject: "Online Settlement #{Date.today.strftime("%B")} (Balance as of #{Date.today}) (Normally sent to #{emails.join(',')}")
   end
 end
