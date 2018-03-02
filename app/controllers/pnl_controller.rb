e3ab8faf814f601a63d3c46529809c9186af03f1
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2018-03-02 17:54:09 -0500

    added stubs to controller

--- app/controllers/pnl_controller.rb
+++ app/controllers/pnl_controller.rb
@@ -1,5 +1,20 @@
 class PnlController < ApplicationController
-  def index
+  def daily
+    # TODO this should accepts some url params when denote the year and month
+    # Query for that year and month
+  end
+
+  def monthly
+    # TODO this should accepts some url params when denote the year
+    # Group by month
+  end
+
+  def yearly
+    # Group by month
+  end
+
+  def export
+    # Generic table export. Keep data structures between periods consistent
   end
 
   def fetch
