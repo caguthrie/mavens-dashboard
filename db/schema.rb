119f1753ac0ee2ea3c29d24c73ad74fe69062ee4
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2018-03-02 17:45:11 -0500

    Adding pnl model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224153952) do
+ActiveRecord::Schema.define(version: 20180302201634) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "player_id"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20180224153952) do
     t.boolean  "going_to_game"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "pnls", force: :cascade do |t|
+    t.string   "username"
+    t.date     "date"
+    t.integer  "balance"
+    t.integer  "transfer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
