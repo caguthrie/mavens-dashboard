f85b7917d24d143e3dc7c87e1e8034a9ae265716
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2018-03-06 23:00:39 -0500

    pnls working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @latest_date = Pnl.order('date DESC').limit(1).first.date
   end
 end
--- app/controllers/pnl_controller.rb
+++ app/controllers/pnl_controller.rb
@@ -5,8 +5,6 @@ class PnlController < ApplicationController
   end
 
   def daily
-    @month = params[:month]
-    @year = params[:year]
     raw_data = Pnl.where("cast(strftime('%m', date) as int) = ?", params[:month].to_i).where("cast(strftime('%Y', date) as int) = ?", params[:year].to_i)
     grouped_data = raw_data.group_by{|row| row.username}.delete_if{|k,v| v.all?{|item| item.amount == 0}}
     @headers = raw_data.map{|d| d.date}.uniq.sort
@@ -22,16 +20,47 @@ class PnlController < ApplicationController
   end
 
   def monthly
-    # TODO this should accepts some url params when denote the year
-    # Group by month
+    raw_data = Pnl.where("cast(strftime('%Y', date) as int) = ?", params[:year].to_i)
+    grouped_dates = raw_data.group_by {|d| d.date.beginning_of_month }
+    @headers = grouped_dates.map{|k,v| k}.sort
+    grouped_by_plaer_and_month = ActiveRecord::Base.connection.execute(
+        "Select player_id, sum(amount) as amount, strftime('%m', date) as month " +
+        "from pnls group by strftime('%m', date), player_id"
+    )
+
+    @rows = []
+    grouped_by_plaer_and_month.group_by{|r| r['player_id']}.reject{|k,v| v.all?{|item| item['amount'] == 0}}.each do |player_id, grouped_amount_and_dates|
+      player = Player.find(player_id)
+      ret = {}
+      ret['player'] = player
+      grouped_amount_and_dates.each do |item|
+        ret[item['month']] = item
+      end
+      @rows.push ret
+    end
+    @rows.sort!{|a, b| a['player'].real_name <=> b['player'].real_name}
   end
 
   def yearly
-    # Group by month
-  end
+    raw_data = Pnl.all
+    grouped_dates = raw_data.group_by {|d| d.date.beginning_of_year }
+    @headers = grouped_dates.map{|k,v| k}.sort
+    grouped_by_plaer_and_month = ActiveRecord::Base.connection.execute(
+        "Select player_id, sum(amount) as amount, strftime('%Y', date) as year " +
+            "from pnls group by strftime('%Y', date), player_id"
+    )
 
-  def export
-    # Generic table export. Keep data structures between periods consistent
+    @rows = []
+    grouped_by_plaer_and_month.group_by{|r| r['player_id']}.reject{|k,v| v.all?{|item| item['amount'] == 0}}.each do |player_id, grouped_amount_and_dates|
+      player = Player.find(player_id)
+      ret = {}
+      ret['player'] = player
+      grouped_amount_and_dates.each do |item|
+        ret[item['year']] = item
+      end
+      @rows.push ret
+    end
+    @rows.sort!{|a, b| a['player'].real_name <=> b['player'].real_name}
   end
 
   def fetch
